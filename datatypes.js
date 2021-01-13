950b2c76ac3c92a40e278e192b0f4ca999530969
peeradech12 <6206021612105@fitm.kmutnb.ac.th>
2021-01-13 08:56:39 +0700

    create datatypes.js

---
+++ datatypes.js
@@ -0,0 +1,17 @@
+const people =["Aaron", "Mel", "John"];
+const one = 1;
+const str = "Helloo World";
+const b = true;
+const ewployee = {
+    firstName: "Peeradech",
+    lastName: "Sadsum-Ang",  
+};
+
+function sayHello(person) {
+    console.log("Hello" + person.firstName);
+}
+
+console.log(typeof people);
+console.log(typeof sayHello)
+console.log(employee instanceof Array);
+sayHello(employee);
